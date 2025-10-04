e454965711824df2f2551415be31265428fa837e
Avinash verma <code6969nation@gmail.com>
2025-10-05 03:27:44 +0530

    initial

---
+++ next.config.js
@@ -0,0 +1,38 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+    domains: [
+      "source.unsplash.com",
+      "images.unsplash.com",
+      "ext.same-assets.com",
+      "ugc.same-assets.com",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ext.same-assets.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ugc.same-assets.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+// module.exports = nextConfig; // Use this for CommonJS (recommended for Netlify)
+
+export default nextConfig; // Use this only if package.json has "type": "module"
